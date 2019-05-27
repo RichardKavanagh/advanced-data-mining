4fa345649c1457b1f7a08f91328e2735831d742d
RichardKavanagh <richard.kavanagh7@mail.dcu.ie>
2019-05-27 20:54:06 +0100

    Updated intro lab with basic models

--- labs/intro_to_classification/intro_to_classification.R
+++ labs/intro_to_classification/intro_to_classification.R
@@ -1,5 +1,7 @@
 # Richard Kavanagh 23/05/19
 
+library(ggplot2)
+library(reshape)
 
 work_dir <- "C:\\workspace\\advanced_data_mining\\datasets"
 setwd(work_dir)
@@ -22,3 +24,52 @@ barplot(table(y), main = "Distribution of Titanic Surivial", ylab="Frequency")
 # randomly select 25% of dataset for testing
 index <-sample(1:length(y),length(y) * .25, replace=FALSE)
 testing <- y[index]
+
+
+# Model 1: Everyone perishes, we just need to know how many No values to produce
+perish_model <- rep("No", length(testing))
+
+# Coin-toss model 
+coin_model <- round(runif(length(testing), min=0, max=1))
+coin_model <- factor(coin_model, levels = c(0,1), labels = c("No", "Yes"))
+
+table(testing, perish_model)
+table(testing, coin_model)
+
+# Convert into % of correctly classified instances i.e prediction accuracy
+(coin_accuracy <- 1 - mean(coin_model != testing))
+
+(perish_accuracy <- 1 - mean(perish_model != testing))
+
+
+# distribution of our sample
+prop.table(table(testing))
+
+
+# build and evaluate 1000 models
+
+perish <- c()
+coin <- c()
+
+for (i in 1:1000) {
+  
+  index <- sample(1:length(y), length(y) * .25, replace = FALSE)
+  testing <- y[index]
+  
+  coin_model <- round(runif(length(testing), min=0, max=1))
+  coin_model <- factor(coin_model, levels = c(0,1), labels = c("No", "Yes"))
+  
+  coin[i] <- mean(coin_model != testing)
+  perish[i] <- mean(perish_model != testing)
+  
+}
+
+results  <- data.frame(coin, perish)
+names(results) <- c("Coin Toss Accuracy", "Everyone Perishes Accuracy")
+summary(results)
+
+
+ggplot(melt(results), mapping = aes (fill = variable, x = value)) + geom_density (alpha = .5)
+
+
+boxplot(results)
